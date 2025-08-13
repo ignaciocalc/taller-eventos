cd3c606a2eafe03449cf861b1ddb8fc607aa7fe0
ignaciocalc <ignaciocal13@gmail.com>
2025-08-13 20:14:54 -0300

    Mensaje DIV

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+   const div = document.getElementById("divUnico");
+
+   div.addEventListener('click', function() {
+      alert('Hola! Soy el div')
+   })
+})
